72fea26dbad328154e17d0e0f9087db9876d4249
Cameron Daniel <cameron.daniel@megaport.com>
2016-07-23 13:45:27 +1000

    bt

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 var five = require("johnny-five");
-var board = new five.Board();
+
+var opts = {};
+opts.port = process.argv[2] || "";
+
+var board = new five.Board(opts);
 
 board.on("ready", function () {
   var led = new five.Led(13);
